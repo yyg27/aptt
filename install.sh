afff81401d25f1b2bd92cb286340d2b12f1714e5
yyg27 <yyg27@outlook.com>
2026-01-21 19:55:23 +0300

    add some styling

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 #chech fzf
 command -v fzf >/dev/null || { echo ":: Installing fzf..."; sudo apt update -qq && sudo apt install -y fzf; }
 
-echo ":: Installing aptt tools..."
+echo "Installing aptt tools..."
 
 BASE_URL="https://raw.githubusercontent.com/yyg27/aptt/main"
 
 sudo curl -sLo /usr/local/bin/aptt   "$BASE_URL/aptt.sh"   && sudo chmod +x /usr/local/bin/aptt
 sudo curl -sLo /usr/local/bin/apttrm "$BASE_URL/apttrm.sh" && sudo chmod +x /usr/local/bin/apttrm
 
-echo ":: Installation complete."
+echo "Installation complete."
